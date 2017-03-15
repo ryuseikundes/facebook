d4d22613ade81de6aaef70bcb213a2dbd64a8627
松本隆誠 <ryuseimatsumoto@matsumototakashimakoto-no-MacBook-Pro.local>
2017-03-15 14:54:53 +0900

    ppp

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,7 +3,7 @@ class TopicsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_topic, only: [:edit, :update, :destroy]
 
-  
+
 
   def new
     @topic = Topic.new
@@ -14,6 +14,7 @@ class TopicsController < ApplicationController
     @topic.user_id = current_user.id
     if @topic.save
       redirect_to root_path, notice: " 投稿しました！"
+      NoticeMailer.sendmail_topic(@topic).deliver
     else
       render 'new'
     end
