4932350e1869ce197d2582c8b56158208f20e71a
松本隆誠 <ryuseimatsumoto@matsumototakashimakoto-no-MacBook-Pro.local>
2017-03-27 15:22:44 +0900

    メッセージ機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,6 @@ Rails.application.routes.draw do
 
 
 
-
-
-
-  get 'relationships/create'
-
-  get 'relationships/destroy'
-
   root to: "topics#index"
 
   resources :topics do
@@ -25,6 +18,10 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
 
   resources :relationships, only: [:create, :destroy]
+
+  resources :conversations do
+  resources :messages
+  end
 
   if Rails.env.development?
   mount LetterOpenerWeb::Engine, at: "/letter_opener"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327033711) do
+ActiveRecord::Schema.define(version: 20170327052400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,25 @@ ActiveRecord::Schema.define(version: 20170327033711) do
     t.integer  "user_id"
     t.integer  "topic_id"
   end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.boolean  "read",            default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+  end
+
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
 
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
@@ -72,4 +91,6 @@ ActiveRecord::Schema.define(version: 20170327033711) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
 
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
