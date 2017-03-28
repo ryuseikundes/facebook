b98898ad96db7374b4d342280c99b62012689ec2
松本隆誠 <ryuseimatsumoto@matsumototakashimakoto-no-MacBook-Pro.local>
2017-03-28 17:58:45 +0900

    production.rbのhostドメインをAWSに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-config.action_mailer.default_url_options = { host: 'sheltered-river-26223.herokuapp.com' }
+config.action_mailer.default_url_options = { host: '52.69.34.117' }
   ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings =
 {
