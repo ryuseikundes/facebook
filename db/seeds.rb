c8d5e4404ceedfd7afbe9bdd9f2d5ec192293492
松本隆誠 <ryuseimatsumoto@matsumototakashimakoto-no-MacBook-Pro.local>
2017-03-30 08:58:34 +0900

    seedデータ用意

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,17 @@
 n = 4
-y = 131
-while (n <= 20) && (y <=147)
-  Comment.create(
-    content: "test",
-    user_id: n,
-    topic_id: y
-)
-  n = n + 1
-  y = y + 1
-end
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+while n <= 20
+  name = Faker::Pokemon.name
+  email = Faker::Internet.email
+  password = Faker::Internet.password
+  uid = SecureRandom.uuid
+
+  User.create(
+    id: n,
+    name: name,
+    email: email,
+    password: password,
+    uid: uid
+  )
+  n = n + 1
+end
