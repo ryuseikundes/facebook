113e3106130efca08193e839ee905e7280516acc
松本隆誠 <ryuseimatsumoto@matsumototakashimakoto-no-MacBook-Pro.local>
2017-03-29 16:14:52 +0900

    seeds.rbにおいてテストデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
+n = 4
+y = 131
+while (n <= 20) && (y <=147)
+  Comment.create(
+    content: "test",
+    user_id: n,
+    topic_id: y
+)
+  n = n + 1
+  y = y + 1
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
